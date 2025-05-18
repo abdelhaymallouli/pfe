79adbba668accdc08a6fa0ec70be765a4b9c9a7f
abdelhaymallouli <abdelhay.mallouli@gmail.com>
2025-05-18 22:31:32 +0100

    the structure of the project backend and frotend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-3xl font-bold text-blue-600">
+      Tailwind + React + TypeScript + Vite works!
+    </div>
+  );
+}
+export default App;
